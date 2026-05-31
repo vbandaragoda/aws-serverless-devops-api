b26743f5686b6f56eeeda14fcc9c96213d296d30
Vimukthi Bandaragoda <vimukthi.nelanga@gmail.com>
2026-05-31 17:13:40 +0530

    createuserfunction

---
+++ lambdas/createUserFunction.mjs
@@ -0,0 +1,40 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const dynamodb = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event) => {
+
+    const body = JSON.parse(event.body);
+
+    const params = {
+        TableName: "DevOpsUsersTable",
+        Item: {
+            userId: body.userId,
+            name: body.name,
+            email: body.email
+        }
+    };
+
+    try {
+
+        await dynamodb.send(new PutCommand(params));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "User created successfully"
+            })
+        };
+
+    } catch (error) {
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        };
+    }
+};
